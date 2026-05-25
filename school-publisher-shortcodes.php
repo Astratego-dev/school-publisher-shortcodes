c0a4234de01fb47d53940805f2c0786b7dca4ee2
אלי שיינפלד <schonfeld@MacBook-Air-sl-ly.local>
2026-05-25 09:55:46 +0300

    Add minimal WordPress plugin header

---
+++ school-publisher-shortcodes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: School Publisher Shortcodes
+ * Description: Shortcodes for a school-focused publishing website.
+ * Version: 0.1.0
+ * Author: Astratego
+ * Text Domain: school-publisher-shortcodes
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
